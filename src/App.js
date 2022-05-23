63a6e61271d2e928fdd627781c2df2621e0f13fd
waelbenn <waelbenngx@outlook.fr>
2022-05-23 12:58:23 -0800

    first comment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import wael from './wael.jpg'
+ class App extends Component {
+   constructor(props){
+     super(props);
+     this.state={
+       fullName:"wael ben abdallah",
+       bio:"studying web dev at GoMyCode",
+       imgSrc:wael,
+       profession:"student",
+       isWorking:false,
+       show:false,
+       counter:0
+     }
+   }
+  btnShow=()=>this.setState({show:!this.state.show})
+  componentDidMount(){
+    console.log("Component was mounted");
+    this.setState({interval: setInterval(()=>{this.setState({counter: this.state.counter + 1})},1000)});
+  }
+  render() {
+   
+    return (
+      <div className='app-header'>
+        
+          <button onClick={this.btnShow}>show state</button>
+{(this.state.show)&&
+<div><h1>
+hello my name is {this.state.fullName},
+i am a {this.state.profession},
+i am currently {this.state.bio}.
+{this.state.show}
+<img src={this.state.imgSrc}/></h1>
+<h1>{this.state.counter}</h1>
+</div>}
+</div>
+    )
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  }
+  
 }
 
-export default App;
+export default App
